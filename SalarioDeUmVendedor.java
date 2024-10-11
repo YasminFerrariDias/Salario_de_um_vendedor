0105769ab9c7e9f558441c677b36008957446127
Yasmin Ferrari Dias <158753555+YasminFerrariDias@users.noreply.github.com>
2024-10-10 21:12:47 -0300

    Add files via upload

---
+++ SalarioDeUmVendedor.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class SalarioDeUmVendedor {
+  public static void main(String[] args) {
+    Scanner var = new Scanner(System.in);
+    float fixo, total;
+    double conta;
+    String nome;
+    System.out.print("Informe o nome do vendedor: ");
+    nome = var.nextLine();
+    System.out.print("Informe o sálario fixo: ");
+    fixo = var.nextFloat();
+    System.out.print("Informe o total de vendas em dinheiro no mês: ");
+    total = var.nextFloat();
+    conta = fixo + (total*0.15);
+    System.out.print(nome + " recebe de sálario fixo " + fixo + " reais, e o seu sálario final é de " + conta);
+  }
+}
